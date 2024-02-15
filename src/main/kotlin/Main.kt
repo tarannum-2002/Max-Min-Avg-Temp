e97ac8fa3da1d2562f927d4633ace62090de69de
tarannum-2002 <tarannums@sahaj.ai>
2024-02-15 10:22:59 +0530

    initial commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,12 @@
 package org.example
 
 import java.io.*
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicLong;
+
 
 
 fun ProcessData(dataMap:HashMap<String, MutableList<Double>> ){
@@ -10,14 +16,14 @@ fun ProcessData(dataMap:HashMap<String, MutableList<Double>> ){
         print(key)
         print("=")
 
-        print(dataMap.get(key)?.get(0) ?: null)
+        print(dataMap.get(key)?.get(0))
         print("/")
-        var sum = dataMap.get(key)?.get(2) ?: null
-        var count= dataMap.get(key)?.get(3) ?: null
-        var avg = count?.let { sum?.div(it.toInt()) } //check let again
+        val sum = dataMap.get(key)?.get(2)
+        val count= dataMap.get(key)?.get(3)
+        val avg = count?.let { sum?.div(it.toInt()) } //check let again
         print(avg)
         print("/")
-        println(dataMap.get(key)?.get(1) ?: null)
+        println(dataMap.get(key)?.get(1))
 
 
     }
@@ -26,7 +32,7 @@ fun ProcessData(dataMap:HashMap<String, MutableList<Double>> ){
 
 
 
-fun ProcessDataAndSort(dataMap:HashMap<String, MutableList<Double>> ){
+fun ProcessDataAndSort(dataMap:ConcurrentHashMap<String, MutableList<Double>> ){
     val sortedMap= dataMap.keys.sorted()
     for(key in sortedMap){
         print(key)
@@ -56,69 +62,92 @@ fun main() {
     val citiesSet = HashSet<String>()
 
     val filePath = "/Users/tarannums/downloads/measurements.txt"
-    var dataMap : HashMap<String, MutableList<Double>> = HashMap()
+    var dataMap : ConcurrentHashMap<String, MutableList<Double>>? = null
+    val path = Paths.get(filePath)
 
 
-    File(filePath).bufferedReader().use { reader ->
-        var line: String? = reader.readLine()
-        while (line != null) {
-            val parts = line.split(";")
-            val city = parts[0]
-            val temp = parts[1].toDouble()
+    val text = Files.lines(path)
 
-            if (dataMap.contains(city)) {
-                var min = dataMap[city]!!.get(0)
-                var max = dataMap[city]!!.get(1)
-                var sum = dataMap[city]!!.get(2)
-                var count = dataMap[city]!!.get(3)
+//    text.parallel().forEach { x: String? -> println(x) }
 
-                if(min > temp){
-                    min= temp
+//    text.close()
+
+
+
+//    File(filePath).bufferedReader().use { reader ->
+//        var line: String? = reader.readLine()
+//        while (line != null) {
+//            val parts = line.split(";")
+//            val city = parts[0]
+//            val temp = parts[1].toDouble()
+//
+//            if (dataMap.contains(city)) {
+//                var min = dataMap[city]!!.get(0)
+//                var max = dataMap[city]!!.get(1)
+//                var sum = dataMap[city]!!.get(2)
+//                var count = dataMap[city]!!.get(3)
+//
+//                if(min > temp){
+//                    min= temp
+//                }
+//                if(max< temp){
+//                    max = temp
+//                }
+//                sum = sum + temp
+//                count = count+1
+//
+//                dataMap.put(city, mutableListOf(min,max,sum,count))
+//
+//
+//            }
+//            else{
+//
+//                dataMap.put(city, mutableListOf(temp,temp,temp,1.0))
+//                citiesSet.add(city)
+//                println(city)
+//            }
+//
+//            line = reader.readLine()
+//        }
+//        reader.close()
+//        ProcessDataAndSort(dataMap)
+//
+//    }
+    Files.lines(Paths.get(filePath)).parallel().forEach { line ->
+        val parts = line.split(";")
+        val city = parts[0]
+        val temp = parts[1].toDouble()
+
+        if (dataMap != null && dataMap.contains(city)) {
+            var (min, max, sum, count) = dataMap[city]!!
+
+                if (min > temp) {
+                    min = temp
                 }
-                if(max< temp){
+                if (max < temp) {
                     max = temp
                 }
-                sum = sum + temp
-                count = count+1
+                sum += temp
+                count++
 
-                dataMap.put(city, mutableListOf(min,max,sum,count))
-
-
-
-//                val fileName = "${city}.txt"
-//                val outputFile = File(fileName)
-//                PrintWriter(outputFile).use { writer ->
-//
-//                    try {
-//                        val doubleValue = parts[1].toDouble()
-//                        writer.println(doubleValue)
-//                    } catch (e: Exception) {
-//
-//                        e.printStackTrace()
-//                    }
-//                }
-
-            }
-            else{
-
-                dataMap.put(city, mutableListOf(temp,temp,temp,1.0))
+                dataMap[city] = mutableListOf(min, max, sum, count)
+            } else {
+                dataMap?.put(city, mutableListOf(temp, temp, temp, 1.0))
                 citiesSet.add(city)
                 println(city)
             }
 
-            line = reader.readLine()
-        }
-        reader.close()
-        ProcessDataAndSort(dataMap)
 
     }
+
+    if (dataMap != null) {
+        ProcessDataAndSort(dataMap)
+    }
+
 
     val endTime = System.currentTimeMillis()
     val elapsedTime = endTime - startTime
     println(elapsedTime)
-
-
-
 
 }
 
